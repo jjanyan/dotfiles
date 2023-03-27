1e052f3ac5f371af3d7d8589a49230737b02c274
Josh Anyan <github@anyan.org>
2023-03-27 17:22:55 -0500

    Update .config/fish/functions/pg.fish

--- private_dot_config/private_fish/functions/pg.fish
+++ private_dot_config/private_fish/functions/pg.fish
@@ -1,3 +1,20 @@
 function pg
-  rg {$argv[1]} -g '*.py'
+    set -l fixed_strings
+    # thanks chatGPT
+
+    if not argparse "f/fixed-strings" -- $argv 2>/dev/null
+        echo "Usage: pg [-f | --fixed] <search_string>"
+        return 1
+    end
+
+    if test -n "$_flag_fixed_strings"
+        set fixed_strings "--fixed-strings"
+    end
+
+    if test (count $argv) -eq 0
+        echo "Usage: pg [-f | --fixed] <search_string>"
+        return 1
+    end
+
+    rg $fixed_strings --type py -- $argv
 end
